98728ab16f8b2ef048e7369ccf3a4b09ea4e5564
David Ochsner <davidochsner93@gmail.com>
2025-11-30 09:26:20 +0100

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "gleam for Advent of Code 2025";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              erlang_28
+              gleam
+            ];
+          };
+      }
+    );
+}
